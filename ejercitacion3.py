2f39e6796017538e9af2371366459e283ab97792
Gabirolda <gabi.lozano93@gmail.com>
2025-06-06 21:00:22 -0300

    cambios

--- ejercitacion3.py
+++ ejercitacion3.py
@@ -5,7 +5,7 @@ numero2 = int(input("Ingrese un numero:"))
 numero3 = int(input("Ingrese un numero:"))
 
 if numero1 > numero2 and numero1 > numero3:
-    print(numero1)
+    print("El numero mayor es: " ,numero1, )
 elif numero2 > numero1 and numero2 >numero3:
-    print(numero2)
-else: print(numero3)
+    print("El numero mayor es: " ,numero2,)
+else: print("El numero mayor es: " ,numero3,)
